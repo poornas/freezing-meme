c30487542fce19f0716b0f9cf92eeaa80aceac3f
Poorna Krishnamoorthy <poorna@stanford.edu>
2013-06-19 14:36:53 -0700

    integrated residemenu

---
+++ SimpleProj/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  SimpleProj
+//
+//  Created by Poorna Krishnamoorthy on 6/19/13.
+//  Copyright (c) 2013 Poorna Krishnamoorthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ SimpleProj/SecondViewController.m
@@ -0,0 +1,38 @@
+//
+//  SecondViewController.m
+//  SimpleProj
+//
+//  Created by Poorna Krishnamoorthy on 6/19/13.
+//  Copyright (c) 2013 Poorna Krishnamoorthy. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
