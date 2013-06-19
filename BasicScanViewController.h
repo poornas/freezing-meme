29b104fa0112d6f1665c40431eb766a90638d3ba
Poorna Krishnamoorthy <poorna@stanford.edu>
2013-06-19 14:42:32 -0700

    integrated OBD2kit

---
+++ BasicScanViewController.h
@@ -0,0 +1,13 @@
+//
+//  BasicScanViewController.h
+//  SimpleProj
+//
+//  Created by Poorna Krishnamoorthy on 6/19/13.
+//  Copyright (c) 2013 Poorna Krishnamoorthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BasicScanViewController : UIViewController
+
+@end
